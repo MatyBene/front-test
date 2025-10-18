8f55d218201e2db9733df3054f062eeef9519876
Matias Benedetti <benedettimaty@gmail.com>
2025-10-18 15:40:03 -0300

    crea auth-service

--- front/src/app/pages/login-page/login-page.ts
+++ front/src/app/pages/login-page/login-page.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login-page.html',
   styleUrl: './login-page.css'
 })
 export class LoginPage {
+  loginForm: FormGroup;
 
+  constructor(private fb: FormBuilder){
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    })
+  }
+
+  onSubmit(){
+    if(this.loginForm.valid){
+      const {username, password} = this.loginForm.value;
+    }
+  }
 }
