b7c58155314ba29bbb49307afdae7515d8e855ba
Matias Benedetti <benedettimaty@gmail.com>
2025-10-18 15:54:19 -0300

    crea model auth donde estan LoginRequest y LoginResponse

---
+++ front/src/app/models/auth.ts
@@ -0,0 +1,8 @@
+export default interface LoginRequest{
+    username: string,
+    password: string
+}
+
+export default interface LoginResponse{
+    token: string
+}
--- front/src/app/services/auth-service.ts
+++ front/src/app/services/auth-service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  
+  private readonly URL = `${environment.apiUrl}/public/login`;
+
+  constructor(private http: HttpClient){}
+
+  login(username: string, password: string){}
 }
