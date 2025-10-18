2a8b7bf5b7ffd82c7a8adeffff38c8aada271d0c
Matias Benedetti <benedettimaty@gmail.com>
2025-10-18 19:46:50 -0300

    crea componente profile y agrega ruta

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomePage } from './pages/home-page/home-page';
 import { LoginPage } from './pages/login-page/login-page';
+import { ProfilePage } from './pages/profile-page/profile-page';
 
 export const routes: Routes = [
     {path: '', component: HomePage},
-    {path: 'login', component: LoginPage}
+    {path: 'login', component: LoginPage},
+    {path: 'profile', component: ProfilePage}
 ];
