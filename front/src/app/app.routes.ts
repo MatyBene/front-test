da292b4c7849879308fee00c1d317e86363e5a87
Matias Benedetti <benedettimaty@gmail.com>
2025-10-19 00:14:34 -0300

    crea auth-guard, si queres entrar al perfil y no estas logueado te manda al login

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -3,11 +3,12 @@ import { HomePage } from './pages/home-page/home-page';
 import { LoginPage } from './pages/login-page/login-page';
 import { ProfilePage } from './pages/profile-page/profile-page';
 import { guestGuard } from './guards/guest-guard';
+import { authGuard } from './guards/auth-guard';
 
 export const routes: Routes = [
     {path: '', component: HomePage},
     {path: 'login', component: LoginPage, canActivate: [guestGuard]},
 
 
-    {path: 'members/profile', component: ProfilePage}
+    {path: 'members/profile', component: ProfilePage, canActivate: [authGuard]}
 ];
