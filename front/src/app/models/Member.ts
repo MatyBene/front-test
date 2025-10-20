71ced2a6d1e323461eb73b41a2101c809048fc0b
Matias Benedetti <benedettimaty@gmail.com>
2025-10-20 10:59:16 -0300

    crea model Member y member-service

---
+++ front/src/app/models/Member.ts
@@ -0,0 +1,10 @@
+export interface Member {
+    name: string,
+    lastname: string,
+    dni: string,
+    birthdate: string,
+    phone: string,
+    email: string,
+    username: string,
+    password: string   
+}
