8698a27c4a4f12ae76fc2d45d6709c2b6277c27c
Matias Benedetti <benedettimaty@gmail.com>
2025-10-20 10:02:11 -0300

    agrega validaciones en form-page.ts

--- front/src/app/pages/form-page/form-page.ts
+++ front/src/app/pages/form-page/form-page.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-form-page',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './form-page.html',
   styleUrl: './form-page.css'
 })
-export class FormPage {
+export class FormPage implements OnInit{
+  userForm!: FormGroup;
+  userId?: string;
 
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private route: ActivatedRoute
+  ){}
+
+  ngOnInit(): void {
+    this.userForm = this.fb.group({
+      name: ['', Validators.required],
+      lastname: ['', Validators.required],
+      dni: ['', [Validators.required, Validators.minLength(8)]],
+      birthdate: ['', [Validators.required, Validators.pattern(/^\d{4}-\d{2}-\d{2}$/)]],
+      phone: ['', [Validators.required, Validators.maxLength(15)]],
+      email: ['', [Validators.required, Validators.email]],
+      username: ['', Validators.required],
+      password: ['', [Validators.required, Validators.pattern(/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?=\S+$).{8,}$/)]]
+    });
+
+    this.userId = this.route.snapshot.params['id'];
+    
+  }
 }
