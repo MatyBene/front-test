6f53b669efaed6b01db13b4a78324dc3d819a1bb
Matias Benedetti <benedettimaty@gmail.com>
2025-10-20 12:16:33 -0300

    agrega validaciones y muestra validaciones del back cuando ya esta registrado un campo unico

--- front/src/app/pages/form-page/form-page.ts
+++ front/src/app/pages/form-page/form-page.ts
@@ -1,18 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
+import { MemberService } from '../../services/member-service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-form-page',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './form-page.html',
   styleUrl: './form-page.css'
 })
 export class FormPage implements OnInit{
   userForm!: FormGroup;
   userId?: string;
+  serverErrors: { [key: string]: string } = {};
 
   constructor(
+    private memberService: MemberService,
     private fb: FormBuilder,
     private router: Router,
     private route: ActivatedRoute
@@ -30,7 +34,48 @@ export class FormPage implements OnInit{
       password: ['', [Validators.required, Validators.pattern(/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?=\S+$).{8,}$/)]]
     });
 
-    this.userId = this.route.snapshot.params['id'];
-    
+    // this.userId = this.route.snapshot.params['id'];
+
+    this.userForm.valueChanges.subscribe(() => {
+      Object.keys(this.userForm.controls).forEach(key => {
+        const control = this.userForm.get(key);
+        if (control?.dirty && this.serverErrors[key]) {
+          delete this.serverErrors[key];
+        }
+      });
+    });
+
+  }
+
+  get name() { return this.userForm.get('name'); }
+  get lastname() { return this.userForm.get('lastname'); }
+  get dni() { return this.userForm.get('dni'); }
+  get birthdate() { return this.userForm.get('birthdate'); }
+  get phone() { return this.userForm.get('phone'); }
+  get email() { return this.userForm.get('email'); }
+  get username() { return this.userForm.get('username'); }
+  get password() { return this.userForm.get('password'); }
+
+  onSubmit(): void {
+    this.memberService.register(this.userForm.value).subscribe({
+      next: (data) => {
+        this.router.navigate(['/public/login']);
+      },
+      error: (e) => {
+        let errorData = e.error;
+        if (typeof e.error === 'string') {
+          try {
+            errorData = JSON.parse(e.error);
+          } catch (parseError) {
+            console.log('No se pudo parsear el error');
+          }
+        }
+        
+        if (errorData?.details?.field && errorData?.details?.message) {
+          this.serverErrors[errorData.details.field] = errorData.details.message;
+          console.log("Errores actualizados:", this.serverErrors);
+        }
+      }
+    });
   }
 }
--- front/src/app/services/member-service.ts
+++ front/src/app/services/member-service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Member } from '../models/Member';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MemberService {
-  
+  private readonly URL = `${environment.apiUrl}/public/register`;
+
+  constructor(private http: HttpClient){}
+
+  register(member: Member) {
+    return this.http.post(this.URL, member, {
+      responseType: 'text'
+    });
+  }
 }
